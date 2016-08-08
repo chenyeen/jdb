e6fc3aba5f48de9d101c3d0cd183d019822f8ef9
张弓 <sz9214e@qq.com>
2016-08-08 12:31:01 +0800

    完成编写新的数据表保存代码

--- src/main/java/cn/cerc/jdb/core/TableOperation.java
+++ src/main/java/cn/cerc/jdb/core/TableOperation.java
@@ -1,306 +1,201 @@
 package cn.cerc.jdb.core;
 
-import static cn.cerc.jdb.other.utils.roundTo;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
-import cn.cerc.jdb.other.DelphiException;
-
 public class TableOperation {
 	private static final Logger log = Logger.getLogger(TableOperation.class);
-	private final String uid = "UID_";
+	private final String CONST_UID = "UID_";
 	private Connection conn;
 	private String tableName;
 	private String lastCommand;
 	private boolean preview = false;
+	private List<String> primaryKeys = new ArrayList<>();
 
 	public TableOperation(Connection connection) {
 		this.conn = connection;
 	}
 
 	public boolean insert(Record record) {
-
-		StringBuffer sql = new StringBuffer();
-		sql.append("insert into ").append(tableName);
-
-		Set<String> fs = record.getItems().keySet();
-		List<String> fileds = new ArrayList<String>(fs);
-		// Logger.debug(getClass(),"fielsd == " + fileds);
-		if (fileds == null || fileds.size() == 0) {
+		if (record.getFieldDefs().size() == 0)
 			throw new RuntimeException("字段为空");
-		}
 
-		// 1、fileds中存在UID_字段 但赋值为空
-		// 2、fileds不存在UID_ 但表KEY中存在 UID_
-		// 标识是否需要从数据库中获取 自增字段 默认为false
-		boolean flag = false;
-		for (int i = 1; i <= fileds.size(); i++) {
-			String key = fileds.get(i - 1);
-			if (uid.equalsIgnoreCase(key)) {
-				if (record.getField(key) != null) {
-					if (isBlank(record.getField(key).toString())) {
-						flag = true;
-						break;
-					}
+		try (BuildStatement bs = new BuildStatement(conn);) {
+			if (primaryKeys.size() == 0)
+				initPrimaryKeys(record);
+			boolean useUID = primaryKeys.contains(CONST_UID) && record.getField(CONST_UID) == null;
+
+			bs.append("insert into ").append(tableName).append(" (");
+			int i = 0;
+			for (String field : record.getFieldDefs().getFields()) {
+				if (!useUID || !CONST_UID.equals(field)) {
+					i++;
+					if (i > 1)
+						bs.append(",");
+					bs.append(field);
 				}
 			}
-		}
-
-		if (!flag) {
-			String pkkey = getKeyByDict(conn, tableName, record);
-			String[] pks = pkkey.split(";");
-
-			for (String pk : pks) {
-				if (uid.equalsIgnoreCase(pk)) {
-					flag = true;
-					break;
+			bs.append(") values (");
+			i = 0;
+			for (String field : record.getFieldDefs().getFields()) {
+				if (!useUID || !CONST_UID.equals(field)) {
+					i++;
+					if (i == 1)
+						bs.append("?", record.getField(field));
+					else
+						bs.append(",?", record.getField(field));
 				}
 			}
-		}
+			bs.append(")");
 
-		StringBuffer filedssql = new StringBuffer();
-		StringBuffer values = new StringBuffer();
-
-		for (String filed : fileds) {
-			if (flag && uid.equalsIgnoreCase(filed)) { // uid值为空 使用mysql自增 此处不处理
-				continue;
-			}
-			filedssql.append(",").append(filed);
-			values.append(",?");
-		}
-
-		if (filedssql.length() > 0) {
-			filedssql.deleteCharAt(0);
-			values.deleteCharAt(0);
-		}
-
-		sql.append("(").append(filedssql).append(") values(").append(values).append(")");
-
-		PreparedStatement ps = null;
-
-		try {
-			// Logger.debug(getClass(),"待保存的记录值: " + tRecord);
-			ps = conn.prepareStatement(sql.toString());
-			for (int i = 1; i <= fileds.size(); i++) {
-				String key = fileds.get(i - 1);
-				Object value = record.getField(key);
-				// uid值为空 使用mysql自增，不处理
-				if (flag && uid.equalsIgnoreCase(key)) {
-					continue;
-				}
-				if (value instanceof TDateTime)
-					ps.setObject(i, ((TDateTime) value).getData());
-				else
-					ps.setObject(i, value);
-			}
-			log.debug("insert：" + ps.toString());
+			PreparedStatement ps = bs.build();
 			lastCommand = ps.toString().split(":")[1].trim();
 			if (preview) {
 				log.info(lastCommand);
 				return false;
-			}
+			} else
+				log.debug(lastCommand);
+
 			int result = ps.executeUpdate();
 
-			if (flag) {
+			if (useUID) {
 				int uidvalue = findAutoUid(conn);
 				log.debug("自增列uid value：" + uidvalue);
-				record.setField(uid, uidvalue);
+				record.setField(CONST_UID, uidvalue);
 			}
 
 			return result > 0;
-		} catch (Exception e) {
-			DelphiException err = new DelphiException(e.getMessage());
-			log.debug("insert sql error: " + sql.toString());
-			err.addSuppressed(e);
-			throw err;
-		} finally {
-			try {
-				if (ps != null) {
-					ps.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new RuntimeException(e.getMessage());
-			}
+		} catch (SQLException e) {
+			log.error(lastCommand);
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
 		}
 	}
 
 	public boolean update(Record record) {
-
-		StringBuffer sql = new StringBuffer();
-		sql.append("update ").append(tableName).append(" set ");
-
-		// List<String> fileds = tRecord.getFieldDefs().getFields();
-		List<String> fileds = new ArrayList<String>(record.getDelta().keySet());
-
-		if (fileds == null || fileds.size() == 0) {
+		Map<String, Object> delta = record.getDelta();
+		if (delta.size() == 0)
 			return true;
-		}
 
-		StringBuffer filedssql = new StringBuffer();
-		StringBuffer wheresqls = new StringBuffer();
-
-		for (String filed : fileds) {
-			filedssql.append(",").append(filed).append(" = ? ");
-
-			// 旧指
-			Object oldVlaue = record.getOldField(filed);
-			// 如果旧值为空
-			if (oldVlaue == null) {
-				wheresqls.append(" and ").append(filed).append(" is null ");
-			} else {
-				wheresqls.append(" and ").append(filed).append(" = ? ");
+		try (BuildStatement bs = new BuildStatement(this.conn);) {
+			if (this.primaryKeys.size() == 0)
+				initPrimaryKeys(record);
+			bs.append("update ").append(tableName);
+			// 加入set条件
+			int i = 0;
+			for (String field : delta.keySet()) {
+				i++;
+				bs.append(i == 1 ? " set " : ",");
+				bs.append(field);
+				bs.append("=?", record.getField(field));
 			}
-		}
-
-		if (filedssql.length() > 0) {
-			filedssql.deleteCharAt(0);
-		}
-
-		sql.append(filedssql);
-
-		sql.append(" where 1 = 1 ");
-
-		List<String> pklist = new ArrayList<String>();
-		String pkkey = getKeyByDict(conn, tableName, record);
-		String[] pks = pkkey.split(";");
-
-		for (String pk : pks) {
-			pklist.add(pk);
-			if (!fileds.contains(pk)) { // 如果更新字段里面不包含主键 需自动将主键加入where条件中
-				wheresqls.append(" and ").append(pk).append(" = ? ");
+			// 加入where条件
+			i = 0;
+			for (String field : primaryKeys) {
+				i++;
+				bs.append(i == 1 ? " where " : " and ").append(field);
+				Object value = delta.containsKey(field) ? delta.get(field) : record.getField(field);
+				if (value != null) {
+					bs.append("=?", value);
+				} else
+					bs.append(" is null ");
 			}
-		}
-
-		sql.append(wheresqls);
-
-		int whereindex = fileds.size() + 1; // where条件后index
-		try (PreparedStatement ps = conn.prepareStatement(sql.toString());) {
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			for (int i = 1; i <= fileds.size(); i++) {
-				String field = fileds.get(i - 1);
-				Object value = record.getField(field);
-				if (value instanceof TDateTime) {
-					ps.setObject(i, record.getField(field).toString());
-				} else if (value instanceof Double) {
-					ps.setObject(i, roundTo((Double) record.getField(field), -6));
-				} else {
-					Object val = record.getField(field);
-					if (val instanceof Date)
-						ps.setObject(i, sdf.format(val));
-					else
-						ps.setObject(i, val); // set 新值
+			for (String field : delta.keySet()) {
+				if (!primaryKeys.contains(field)) {
+					i++;
+					bs.append(i == 1 ? " where " : " and ").append(field);
+					Object value = delta.get(field);
+					if (value != null) {
+						bs.append("=?", value);
+					} else
+						bs.append(" is null ");
 				}
-				// 旧值
-				Object oldVlaue = record.getOldField(field);
-
-				if (pklist.contains(field)) { // 如果为主键 如果delta里值为空 ，则取datas里值
-												// 此时datas里值不允许为空
-					if (oldVlaue == null) {
-						if (value == null) {
-							log.debug("tableName: " + tableName);
-							log.debug("records: " + record.toString());
-							throw new RuntimeException("修改主键不允许为空");
-						} else {
-							oldVlaue = value;
-						}
-					}
-
-				}
-				if (oldVlaue != null) {
-					if (oldVlaue instanceof Double) {
-						ps.setObject(whereindex, roundTo((Double) oldVlaue, -6));
-					} else {
-						// set 值
-						if (oldVlaue instanceof Date)
-							ps.setObject(whereindex, sdf.format(oldVlaue));
-						else
-							ps.setObject(whereindex, oldVlaue); // set 值
-					}
-					whereindex++;
-				}
-				pklist.remove(field); // 为后续主键获取拼接条件
 			}
 
-			for (String pk : pklist) { // 如果这个集合中 还存在主键 则说明更新字段缺少主键 需自动设置值
-										// 值从datas里面获取 不允许为空
-				Object value = record.getField(pk);
-				if (value == null) {
-					log.debug("tableName: " + tableName);
-					log.debug("records: " + record.toString());
-					throw new RuntimeException("修改主键不允许为空");
-				}
-				if (value instanceof TDateTime) {
-					ps.setObject(whereindex, value.toString());
-				} else if (value instanceof Double) {
-					double tmp = roundTo((Double) value, -6);
-					ps.setObject(whereindex, tmp);
-				} else {
-					ps.setObject(whereindex, value);
-				}
-				whereindex++;
-			}
+			PreparedStatement ps = bs.build();
 			lastCommand = ps.toString().split(":")[1].trim();
 			if (preview) {
 				log.info(lastCommand);
 				return false;
 			}
+
 			if (ps.executeUpdate() != 1) {
-				log.error("update error：" + ps.toString());
+				log.error(lastCommand);
 				throw new RuntimeException("当前记录已被其它用户修改或不存在，更新失败");
 			} else {
-				log.debug("update：" + ps.toString());
+				log.debug(lastCommand);
 				record.getDelta().clear();
 				return true;
 			}
-		} catch (Exception e) {
+		} catch (SQLException e) {
+			log.error(lastCommand);
 			e.printStackTrace();
 			throw new RuntimeException(e.getMessage());
 		}
 	}
 
 	public boolean delete(Record record) {
-		StringBuffer sql = new StringBuffer();
+		try (BuildStatement bs = new BuildStatement(conn);) {
+			if (this.primaryKeys.size() == 0)
+				initPrimaryKeys(record);
 
-		sql.append("delete from ").append(tableName).append(" where 1 = 1 ");
-
-		String key = getKeyByDict(conn, tableName, record);
-		String[] pks = key.split(";");
-
-		for (String pk : pks) {
-			sql.append(" and ").append(pk).append(" = ? ");
-		}
-
-		try (PreparedStatement ps = conn.prepareStatement(sql.toString());) {
-			Object value = null;
-			int index = 1;
-			for (String pk : pks) {
-				value = record.getField(pk);
-				if (value == null) {
+			bs.append("delete from ").append(tableName);
+			int i = 0;
+			Map<String, Object> delta = record.getDelta();
+			for (String pk : primaryKeys) {
+				Object value = delta.containsKey(pk) ? delta.get(pk) : record.getField(pk);
+				if (value == null)
 					throw new RuntimeException("主键值为空");
-				}
-				ps.setObject(index, value);
-				index++;
+				i++;
+				bs.append(i == 1 ? " where " : " and ");
+				bs.append(pk).append("=? ", value);
 			}
-			log.debug("delete：" + ps.toString());
+
+			PreparedStatement ps = bs.build();
 			lastCommand = ps.toString().split(":")[1].trim();
 			if (preview) {
 				log.info(lastCommand);
 				return false;
-			}
+			} else
+				log.debug(lastCommand);
+
 			return ps.execute();
-		} catch (Exception e) {
+		} catch (SQLException e) {
+			log.error(lastCommand);
+			e.printStackTrace();
 			throw new RuntimeException(e.getMessage());
+		}
+	}
+
+	private void initPrimaryKeys(Record record) throws SQLException {
+		for (String key : record.getFieldDefs().getFields()) {
+			if (CONST_UID.equalsIgnoreCase(key)) {
+				if (!CONST_UID.equals(key))
+					throw new RuntimeException(String.format("%s <> %s", CONST_UID, key));
+				primaryKeys.add(CONST_UID);
+				break;
+			}
+		}
+		if (primaryKeys.size() == 0) {
+			String[] pks = getKeyByDB(conn, tableName).split(";");
+			if (pks.length == 0)
+				throw new RuntimeException("获取不到主键PK");
+			for (String pk : pks) {
+				if (CONST_UID.equalsIgnoreCase(pk)) {
+					if (!CONST_UID.equals(pk))
+						throw new RuntimeException(String.format("%s <> %s", CONST_UID, pk));
+					break;
+				}
+				primaryKeys.add(pk);
+			}
 		}
 	}
 
@@ -342,57 +237,22 @@ public class TableOperation {
 		return result.intValue();
 	}
 
-	private String getKeyByDict(Connection conn, String tableName, Record tRecord) {
-		// 默认情况
-		if (tRecord.getFieldDefs().getFields().contains(uid)) {
-			return uid;
-		}
-
-		// 配置文件
-		String key = DatabaseDict.get(tableName);
-		if (!isBlank(key)) {
-			return key;
-		}
-
-		// 数据库表主键KEY
-		key = getKeyByDB(conn, tableName);
-		if (!isBlank(key)) {
-			return key;
-		}
-
-		throw new RuntimeException("获取不到主键PK");
-
-	}
-
-	private String getKeyByDB(Connection conn, String tableName) {
-		String sql = "select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where table_name= ? AND COLUMN_KEY= 'PRI' ";
-
+	private String getKeyByDB(Connection conn, String tableName) throws SQLException {
 		StringBuffer result = new StringBuffer();
-
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			ps = conn.prepareStatement(sql.toString());
-			ps.setObject(1, tableName);
-
-			rs = ps.executeQuery();
+		try (BuildStatement bs = new BuildStatement(conn);) {
+			bs.append("select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS ");
+			bs.append("where table_name= ? AND COLUMN_KEY= 'PRI' ", tableName);
+			PreparedStatement ps = bs.build();
+			log.warn(ps.toString().split(":")[1].trim());
+			ResultSet rs = ps.executeQuery();
+			int i = 0;
 			while (rs.next()) {
-				result.append(";").append(rs.getString("COLUMN_NAME"));
-			}
-			if (result.length() > 0) {
-				result.deleteCharAt(0);
+				i++;
+				if (i > 1)
+					result.append(";");
+				result.append(rs.getString("COLUMN_NAME"));
 			}
 			return result.toString();
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		} finally {
-			try {
-				if (ps != null) {
-					ps.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
@@ -440,5 +300,9 @@ public class TableOperation {
 
 	public void setPreview(boolean preview) {
 		this.preview = preview;
+	}
+
+	public List<String> getPrimaryKeys() {
+		return primaryKeys;
 	}
 }
--- src/test/java/cn/cerc/jdb/core/TableOperationTest.java
+++ src/test/java/cn/cerc/jdb/core/TableOperationTest.java
@@ -3,43 +3,108 @@ package cn.cerc.jdb.core;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TableOperationTest {
-	private StubConnection conn = new StubConnection();
+	private StubConnection handle = new StubConnection();
+	private int maxTest = 50;
 	private TableOperation obj;
 
 	@Before
 	public void setUp() throws Exception {
-		obj = new TableOperation(conn.getConnection());
+		obj = new TableOperation(handle.getConnection());
 	}
 
 	@Test
-	public void test_insert() {
-		Record record = new Record();
-		record.setField("Code_", "AAA");
-		obj.setPreview(true);
-		obj.setTableName("Dept");
-		obj.insert(record);
+	public void test_1_insert_old() {
+		handle.execute("delete from temp where name_='old'");
+		DataQueryOld ds = new DataQueryOld(handle);
+		ds.setMaximum(0);
+		ds.add("select * from temp");
+		ds.open();
+		for (int i = 0; i < maxTest; i++) {
+			ds.append();
+			ds.setField("Code_", "old" + i);
+			ds.setField("Name_", "old");
+			ds.setField("Value_", i + 1);
+			ds.post();
+		}
 	}
 
 	@Test
-	public void test_update() {
-		Record record = new Record();
-		record.setState(DataSetState.dsEdit);
-		record.setField("Code_", "AAA");
-		obj.setPreview(true);
-		obj.setTableName("Dept");
-		obj.update(record);
+	public void test_2_insert_new() {
+		handle.execute("delete from temp where name_='new'");
+		DataQuery ds = new DataQuery(handle);
+		ds.setMaximum(0);
+		ds.add("select * from temp");
+		ds.open();
+		for (int i = 0; i < maxTest; i++) {
+			ds.append();
+			ds.setField("Code_", "new" + i);
+			ds.setField("Name_", "new");
+			ds.setField("Value_", i + 1);
+			ds.post();
+		}
 	}
 
 	@Test
-	public void test_delete() {
-		Record record = new Record();
-		record.setField("Code_", "AAA");
-		obj.setPreview(true);
-		obj.setTableName("Dept");
-		obj.delete(record);
+	@Ignore
+	public void test_3_insert_new() {
+		// obj.setPreview(true);
+		obj.setTableName("temp");
+		for (int i = 0; i < maxTest; i++) {
+			Record record = new Record();
+			record.getFieldDefs().add("UID_");
+			record.setField("Code_", "code1");
+			record.setField("Name_", "new");
+			record.setField("Value_", i + 1);
+			obj.insert(record);
+		}
+	}
+
+	@Test
+	public void test_3_update_old() {
+		DataQueryOld ds = new DataQueryOld(handle);
+		ds.add("select * from temp");
+		ds.open();
+		while (ds.fetch()) {
+			ds.edit();
+			ds.setField("Code_", ds.getString("Code_") + "a");
+			ds.setField("Value_", ds.getDouble("Value_") + 1);
+			ds.post();
+		}
+	}
+
+	@Test
+	public void test_4_update_new() {
+		DataQuery ds = new DataQuery(handle);
+		ds.add("select * from temp");
+		ds.open();
+		while (ds.fetch()) {
+			ds.edit();
+			ds.setField("Code_", ds.getString("Code_") + "a");
+			ds.setField("Value_", ds.getDouble("Value_") + 1);
+			ds.post();
+		}
+	}
+
+	@Test
+	public void test_5_delete_old() {
+		DataQueryOld ds = new DataQueryOld(handle);
+		ds.add("select * from temp where Name_='old'");
+		ds.open();
+		while (!ds.eof())
+			ds.delete();
+	}
+
+	@Test
+	public void test_6_delete_new() {
+		DataQuery ds = new DataQuery(handle);
+		ds.add("select * from temp where Name_='new'");
+		ds.open();
+		while (!ds.eof())
+			ds.delete();
 	}
 
 	@Test
@@ -55,5 +120,4 @@ public class TableOperationTest {
 		sql = "select * FROM Dept";
 		assertEquals(obj.findTableName(sql), "Dept");
 	}
-
 }
